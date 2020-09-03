7d611bd0b4373e9878e364f4b308529a2927d311
Darren <darren.daley@hotmail.co.uk>
2020-09-03 08:23:28 +0100

    project cleanup

--- Stop.Repositories/Importers/ICSVImporter.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Stop.Repository.Importers
-{
-    public interface ICSVImporter<T>
-    {
-        IEnumerable<T> Import(char seperatorChar, string data);
-    }
-}
